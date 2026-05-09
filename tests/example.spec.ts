04efbe000eb112d0ee7755d1ce16f8c776d30aef
Vu Thi Khanh <40489484+khanhvt1@users.noreply.github.com>
2026-05-09 16:08:30 +0700

    Update English comment example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-//Lesson 1: exercises: a. Access website: https://material.playwrightvn.com/
+//Lesson 1 exercises: a. Access website: https://material.playwrightvn.com/
 test('access web: https://material.playwrightvn.com/', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
-  // Lesson 1: exercises: Check Title contains text "Tài liệu học automation test"
+  // Lesson 1 exercises: Check Title contains text "Tài liệu học automation test"
   await expect(page).toHaveTitle(/Tài liệu học automation test/);
 });
 
@@ -12,9 +12,9 @@ test('access web: https://material.playwrightvn.com/', async ({ page }) => {
 test('get started link', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
-  //Lesson 1 exercises: b. Click vào link chứa text: "Bài học 1: Register Page".
+  //Lesson 1 exercises: b. Click vào link that has the text: "Bài học 1: Register Page".
   await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
 
-  // Lesson 1 exercises: c. Kiểm tra trang web có chứa thẻ heading với nội dung: "User Registration"
+  // Lesson 1 exercises: c. Verify the website has a heading with content: "User Registration"
   await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
 });
